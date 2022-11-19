267e39dd975d125391221ee9c849253c32e32efc
Lou <lou@glidetalk.com>
2022-11-19 10:35:32 +0800

    Initial Commit

---
+++ dotInConverter/dotInConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  dotInConverterApp.swift
+//  dotInConverter
+//
+//  Created by Haotian Lou on 2022/11/19.
+//
+
+import SwiftUI
+
+@main
+struct dotInConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
